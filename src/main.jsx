ce4e7bb2e1e6f272b235a3612a2cfd9335756446
Adarsh K R <kr.adarsh002@gmail.com>
2017-12-18 14:13:23 +0530

    Updated gitignore and deleted node modules

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Home from "./Home";
-
-// document.addEventListener("DOMContentLoaded", function() {
-//   ReactDOM.render(React.createElement(Home), document.getElementById("App"));
-// });
-
-render(<Home />, document.getElementById("App"));
